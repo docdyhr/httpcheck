cacbea8ae8d9584bf0b27f695b7c0295bd8ac379
Thomas Juul Dyhr <thomas@dyhr.com>
2022-01-03 01:54:04 +0100

    small updates various files

--- request_get.py
+++ request_get.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
 import argparse
+import sys
+
 import requests
 
 
 def get_page(url):
+    """Load webpage and return status"""
     try:
         res = requests.get(url, timeout=2)
         return res.status_code
@@ -14,6 +16,7 @@ def get_page(url):
 
 
 def main(argv=sys.argv[1:]):
+    """Get urls from command line return and print status code"""
     parser = argparse.ArgumentParser()
     parser.add_argument('--urlfile', type=argparse.FileType('rt'), default=sys.stdin)
     parser.add_argument('urls', nargs='*')
